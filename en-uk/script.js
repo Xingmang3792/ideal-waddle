93a2843660016450a4837d87665816b8a13c0f58
Xingmang3792 <157291942+Xingmang3792@users.noreply.github.com>
2024-05-18 20:02:22 +0800

    Update script.js

--- en-uk/script.js
+++ en-uk/script.js
@@ -4,6 +4,6 @@ function validatePassword() {
     var correctPassword = "101231"; // 将此处替换为你的密码
 
     if (inputPassword === correctPassword) {
-        alert("I don't want you know sth that I don't want you find is the reason why this iPad's screen password has been set.");
+        alert("I don't want you know sth that you shouldn't find is the reason why this iPad's time-limit password has been set.");;
     }
 }
