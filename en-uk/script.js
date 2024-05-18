2538a9192bb6b215a977ee8c466531fd371bebe2
Xingmang3792 <157291942+Xingmang3792@users.noreply.github.com>
2024-05-18 19:52:57 +0800

    Create script.js

---
+++ en-uk/script.js
@@ -0,0 +1,9 @@
+// script.js
+function validatePassword() {
+    var inputPassword = document.getElementById("passwordInput").value;
+    var correctPassword = "101231"; // 将此处替换为你的密码
+
+    if (inputPassword === correctPassword) {
+        alert("I don't want you know sth that I don't want you find is the reason why this iPad's screen password has been set.");
+    }
+}
